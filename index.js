394945cbe40a69f131077b752c387c8ed00ea7ce
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-04-08 20:50:44 +0530

    some errors are fixed

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use("/user", proxy("http://localhost:4001"));
 app.use("/captain", proxy("http://localhost:4002"));
 app.use("/rides", proxy("http://localhost:4003"));
 
-app.listen(4000, () => {
+app.listen(4000, "0.0.0.0", () => {
     console.log("Gateway is running");
 })
