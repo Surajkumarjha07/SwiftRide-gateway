1ba56e00eb381e3f524138721f546fd914e28188
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-06-08 19:50:08 +0530

    working on payment-service!

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
 import proxy from "express-http-proxy";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+
+const corsOptions = {
+    origin: "*",
+    credentials: true
+}
+
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello! Suraj, I am gateway-service");
@@ -13,6 +21,7 @@ app.use("/user", proxy("http://localhost:4001"));
 app.use("/captain", proxy("http://localhost:4002"));
 app.use("/rides", proxy("http://localhost:4003"));
 app.use("/fare", proxy("http://localhost:4004"));
+app.use("/payment", proxy("http://localhost:4005"));
 
 app.listen(Number(process.env.PORT), "0.0.0.0", () => {
     console.log("Gateway is running");
